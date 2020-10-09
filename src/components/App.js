3bab16f7ce662c6a06064c978505543c7a113270
Joyanta Sarker <35419547+joyantabd@users.noreply.github.com>
2020-10-09 12:53:13 +0600

    React State

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Counter from './Counter/Counter';
+import First from './First/First';
 
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Joyanta Kumer Sarker', email: 'jsarker96@gmail.com', address: 'Professorpara' },
+      { name: 'Sarker Kumar', email: 'kumarsarker@gmail.com', address: 'Palashbari' },
+      { name: 'Kumer Sarker', email: 'kumarsarker@gmail.com', address: 'Gaibandha' }
+    ]
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+
+        {this.state.persons.map((people, index) => {
+          return <First
+            key={index}
+            name={people.name} />
+        })}
      
-     <Counter/>
+        <Counter />
 
   
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
