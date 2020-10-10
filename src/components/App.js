a21ff5c5316e22238b6b6b5d181b78898e2390fc
Joyanta Sarker <35419547+joyantabd@users.noreply.github.com>
2020-10-10 22:52:06 +0600

    Fetch Data From Server using API

--- src/components/App.js
+++ src/components/App.js
@@ -1,74 +1,43 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import './App.css';
-import Books from './Books/Books';
-
-
 
 
 class App extends Component {
 
 
   state = {
-    books: [
-      {
-        id: 1,
-        name: 'JavaScript',
-        price: 50
-      },
-      {
-        id: 2,
-        name: 'React',
-        price: 5
-      },
-      {
-        id: 3,
-        name: 'Node',
-        price: 30
-      },
-      {
-        id: 4,
-        name: 'Express',
-        price: 60
-      },
-  ]
-  }
-  deleteHandler = (id) => {
-    let newBooks = this.state.books.filter(book => book.id !== id)
-    this.setState({
-      books:newBooks
+   posts:[]
+ }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        this.setState({
+          posts: response.data
+        })
+      })
+      .catch(error => {
+      console.log(error)
     })
   }
-
-  editHandler = (name, id) => {
-    let newBooks = this.state.books.map(book => {
-      if (book.id === id) {
-        return {
-          ...book,
-          name
-        }
-      }
-      return book
-    })
-
-    this.setState({
-      books: newBooks
-    })
-
-  }
-
   
 
   render() {
-    return (
-      <div className="App">
 
-        <Books
-          editHandler={this.editHandler.bind(this)}
-          deleteHandler={this.deleteHandler.bind(this)}
-          books={this.state.books} />
-      </div>
-        
-    );
+    let {posts} = this.state
+
+    if (posts.length === 0) {
+     return <h1 style={{textAlign:'center'}}>LOADING</h1>
+    } else {
+      return (
+        <div className='container'>
+          <ul className='list-group'>
+            {posts.map(post => <li key={post.id} className='list-group-item'><h5>Title:</h5>{post.title}<br /><h5>Body:</h5>{post.body}</li>)}
+            </ul>
+        </div>
+      )
+   }
   }
 }
 
