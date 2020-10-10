196899285ffa778d2cbaef2e3c92e55ad631a374
Joyanta Sarker <35419547+joyantabd@users.noreply.github.com>
2020-10-10 12:41:57 +0600

    Stateless Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Staless from './StatelessComponent';
 
 
 
 class App extends Component {
 
-  state= {
-    name: '',
-    click: 0
-  }
-  
-  clickhandeler = () => {
-    alert('I am Clicked')
-    this.setState({
-      click: this.state.click + 1
-    })
-}
-
-  inputHandler = (event) => {
-    this.setState({
-      name: event.target.value
-    })
-  }
 
   render() {
     return (
       <div className="App">
 
-        <h1>
-        <div className='container my-5'>
-          <input onChange={this.inputHandler}  value={this.state.name} type='text' placeholder='Enter Your Name'/><br/>
-        <button className='btn btn-success' onClick={this.clickhandeler}>
-          Click Me
-     </button>
-        </div>
-
-        Number of Button Clicked: {this.state.click}
-     
-        {this.state.name ? <p> Hello Mr,{this.state.name}</p> : '' }
-
-        </h1>
+        <Staless name='React'/>
       </div>
         
     );
@@ -48,4 +20,3 @@ class App extends Component {
 
 export default App;
 
-//Button Event is inLine event Here
---
+++ src/components/StatelessComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Staless = (props) => {
+    return <h1>This is a {props.name} Stateless Component</h1>
+}
+
+export default Staless
